de6016af5572b289da1b5663ab83fe29353e0cc9
xavierbrasher <xavier.brasher@gmail.com>
2024-01-09 00:16:42 +0800

    changed to ts electron tailwind lol

--- cite-app/renderer.js
+++
@@ -1,9 +0,0 @@
-const infomation = document.getElementById("info");
-infomation.innerText = `This app is using chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
-
-const button = document.getElementById("but");
-
-button.addEventListener("click", async () => {
-  button.innerText = await versions.ping();
-  console.log(await versions.ping());
-});
